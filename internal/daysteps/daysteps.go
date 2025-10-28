c4176941961cbe021643e3b31b545b93bda8a141
Eduard <stedi.str@gmal.com>
2025-10-28 15:46:30 +0300

    first commit

--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -1,6 +1,9 @@
 package daysteps
 
 import (
+	"fmt"
+	"strconv"
+	"strings"
 	"time"
 )
 
@@ -12,9 +15,37 @@ const (
 )
 
 func parsePackage(data string) (int, time.Duration, error) {
-	// TODO: реализовать функцию
+	// получаем из data количество шагов и время прогулки
+	str := strings.Split(data, ",")
+	if len(str) != 2 {
+		return 0, 0, fmt.Errorf("slice length not two")
+	}
+	// переводим строку в шаги
+	steps, err := strconv.Atoi(str[0])
+	if err != nil {
+		return 0, 0, err
+	}
+	if steps < 1 {
+		return 0, 0, fmt.Errorf("steps less than one")
+	}
+	// переводим строку в время (duration)
+	duration, err := time.ParseDuration(str[1])
+	if err != nil {
+		return steps, 0, err
+	}
+
+	return steps, duration, nil
 }
 
 func DayActionInfo(data string, weight, height float64) string {
-	// TODO: реализовать функцию
+	steps, duration, err := parsePackage(data)
+	if err != nil {
+		fmt.Println(err)
+		return ""
+	}
+	distance := (float64(steps) * stepLength) / mInKm
+	kcal := WalkingSpentCalories(steps, weight, height, duration)
+	str := fmt.Sprintf("Количество шагов: %d.\nДистанция составила %.2f км.\nВы сожгли %.2f ккал.\n", steps, distance, kcal)
+
+	return str
 }
